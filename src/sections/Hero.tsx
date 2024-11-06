93b246dd666a2117f0c1c8606ea16c8fe0669318
Aniket-Subudh1 <aniketsubudhi00@gmail.com>
2024-11-06 05:31:13 +0530

    Basic Update 1

--- src/sections/Hero.tsx
+++ src/sections/Hero.tsx
@@ -4,13 +4,15 @@ import starsBg from "@/assets/stars.png";
 export const Hero = () => {
   return (
     <section
-      className="h-[492px] min-h-screen flex items-center overflow-hidden relative"
+      className="flex h-[600px] md:h-[800px] items-center overflow-hidden relative [mask-image:linear-gradient(to_bottom,transparent,black_10%,black_90%,transparent)]"
       style={{
         backgroundImage: `url(${starsBg.src})`
       }}
     > 
     <div className="absolute inset-0 bg-[radial-gradient(75%_75%_at_center_center,rgb(140,69,255,.5)_15%,rgb(14,0,36,.5)_78%,transparent)]"></div>
-     <div className="absolute h-64 w-64 bg-purple-500 rounded-full border border-white/20 top-1/2 left-1/2 -translate-x-1/2 -translate-y-1/2 bg-[radial-gradient(50%_50%_at_16.8%_18.3%,white,rgb(184,148,255)_37.7%,rgb(24,0,66))] shadow-[-20px_-20px_50px_rgb(255,255,255,.5),-20px_-20px_80px_rgb(255,255,255,.1),0_0_50px_rgb(140,69,255)]"></div>
+    
+     <div className="absolute h-64 w-64 md:h-96 md:w-96 bg-purple-500 rounded-full border border-white/20 top-1/2 left-1/2 -translate-x-1/2 -translate-y-1/2 bg-[radial-gradient(50%_50%_at_16.8%_18.3%,white,rgb(184,148,255)_37.7%,rgb(24,0,66))] shadow-[-20px_-20px_50px_rgb(255,255,255,.5),-20px_-20px_80px_rgb(255,255,255,.1),0_0_50px_rgb(140,69,255)]"></div>
+    
       <div className="absolute h-[344px] w-[344px] border-white border opacity-20  rounded-full top-1/2 left-1/2 -translate-x-1/2 -translate-y-1/2">
          <div className="absolute h-2 w-2  left-0 bg-white rounded-full  top-1/2 -translate-x-1/2 -translate-y-1/2 "> </div>
          <div className="absolute h-2 w-2  left-1/2 bg-white rounded-full  top-0 -translate-x-1/2 -translate-y-1/2 "> </div>
@@ -19,14 +21,15 @@ export const Hero = () => {
           </div>
       </div>
       <div className="absolute h-[444px] w-[444px] rounded-full border border-white/20 top-1/2 left-1/2 -translate-x-1/2 -translate-y-1/2 border-dashed "></div>
-      <div className="absolute h-[544px] w-[544px] rounded-full border border-white opacity-20 top-1/2 left-1/2 -translate-x-1/2 -translate-y-1/2 border-dashed "></div>
+      <div className="absolute h-[544px] w-[544px] rounded-full border border-white opacity-20 top-1/2 left-1/2 -translate-x-1/2 -translate-y-1/2 border-dashed ">
       <div className="absolute h-2 w-2  left-0 bg-white rounded-full  top-1/2 -translate-x-1/2 -translate-y-1/2 "> </div>
-      <div className="absolute h-2 w-2  left-full bg-white rounded-full  top-1/2 -translate-x-1/2 -translate-y-1/2 "> </div>
+      <div className="absolute h-2 w-2  left-full bg-white rounded-full  top-1/2 -translate-x-1/2 -translate-y-1/2 "> </div></div>
+      
       <div className="container relative mt-28">
-        <h1 className="sm:text-8xl md:text-7xl bg-white lg:text-8xl font-semibold text-center tracking-tighter bg-[radial-gradient(100%_100%_at_top_left,white,white,rgb(74,32,138,.5))] text-transparent bg-clip-text">
-          Coming Soon
+        <h1 className="sm:text-8xl md:text-[168px] md:leading-none bg-white lg:text-8xl font-semibold text-center tracking-tighter bg-[radial-gradient(100%_100%_at_top_left,white,white,rgb(74,32,138,.5))] text-transparent bg-clip-text">
+          Coming <br></br>Soon
         </h1>
-        <p className="text-lg text-white/70 mt-5 text-center">
+        <p className="text-lg md:text-xl text-white/70 mt-5 text-center max-w-xl mx-auto">
           DevSomeware is an open-source community that believes in working and growing together
         </p>
         <div className="flex justify-center mt-5 lg:mt-10">
--- src/sections/Navbar.tsx
+++ src/sections/Navbar.tsx
@@ -5,21 +5,21 @@ export const Navbar = () => {
     return (
       <header className="sticky top-0 left-0  bg-background-1/5 z-[999] w-full backdrop-blur-lg border-b border-white/10">
         <div className="py-2">
-          <div className="container mx-auto px-4 sm:ml-4 lg:ml-40 md:ml-32">
+          <div className="container mx-auto px-4 sm:ml-2 lg:ml-40 md:ml-8 ">
 
             <div className="flex items-center">
             <div className="relative  hover:shadow-[0_0_30px_15px_rgba(140,69,255,0.6)] transition duration-300 ease-in-out p-2 rounded-lg">
                <div className="absolute inset-0 bg-gradient-to-b from-transparent  to-purple-900 opacity-0 hover:opacity-100 transition-opacity duration-300 rounded-lg "></div>
                  <Image src={Logo} alt="DWS Logo" width={110} height={40} className="relative z-10" />
                </div>
-               <button className="relative md:left-0 sm:left-20 py-2 px-3  text-white ml-auto text-xl font-medium rounded-full bg-gradient-to-b from-[#190d2e] to-[#4a208a] shadow-[0px_0px_12px_#8c45ff] hover:shadow-[0px_0px_20px_#8c45ff] transition-all duration-300 ease-in-out overflow-hidden">
-             <div className="absolute inset-0 bg-gradient-to-r from-[#190d2e] via-[#4a208a] to-[#8c45ff] bg-[length:400%_400%] animate-gradientMove">
-              <div className="rounded-full border border-white/20 absolute inset-0 [mask-image:linear-gradient(to_bottom,black,transparent)]"></div>
-              <div className="rounded-full border absolute inset-0 border-white/40 [mask-image:linear-gradient(to_top,black,transparent)]"></div>
-              <div className="absolute inset-0 shadow-[0_0_15px_rgb(140,69,255,0.8)_inset] full animate-glow"></div>
-             </div>
-             <span className="relative z-10">("Join Us")</span>
-            </button>
+                <button className="relative sm:left-18  py-2 px-3  text-white ml-auto text-xl font-medium rounded-full bg-gradient-to-b from-[#190d2e] to-[#4a208a] shadow-[0px_0px_12px_#8c45ff] hover:shadow-[0px_0px_20px_#8c45ff] transition-all duration-300 ease-in-out overflow-hidden">
+                <div className="absolute inset-0 bg-gradient-to-r from-[#190d2e] via-[#4a208a] to-[#8c45ff] bg-[length:400%_400%] animate-gradientMove">
+                <div className="rounded-full border border-white/20 absolute inset-0 [mask-image:linear-gradient(to_bottom,black,transparent)]"></div>
+                <div className="rounded-full border absolute inset-0 border-white/40 [mask-image:linear-gradient(to_top,black,transparent)]"></div>
+               <div className="absolute inset-0 shadow-[0_0_15px_rgb(140,69,255,0.8)_inset] full animate-glow"></div>
+               </div>
+               <span className="relative z-10">("Join Us")</span>
+              </button>
                  
             </div>
           </div>
