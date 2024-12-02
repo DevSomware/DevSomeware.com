34baf8de7e2c58bcc3c47daf89ce85957b613f65
Basir Khan <khanbasir5555@gmail.com>
2024-12-02 15:28:27 +0530

    intial data added

---
+++ src/lib/features/user/userSlice.ts
@@ -0,0 +1,49 @@
+import { createSlice } from '@reduxjs/toolkit'
+
+// Define a type for the slice state
+interface userState {
+  name:string,
+  email:string,
+  github:string,
+  linkedin:string,
+  img:string,
+  intrests:string[],
+  languages:string,
+  frameworks:string,
+  isauth:boolean,
+}
+
+// Define the initial state using that type
+const initialState: userState = {
+    name:"",
+    email:"",
+    github:"",
+    linkedin:"",
+    img:"",
+    intrests:[],
+    languages:"",
+    frameworks:"",
+    isauth:false,
+}
+
+export const userSlice = createSlice({
+  name: 'counter',
+  // `createSlice` will infer the state type from the `initialState` argument
+  initialState,
+  reducers: {
+   add:(state,action)=>{
+    state.name=action.payload.name
+    state.email=action.payload.email
+    state.github=action.payload.github
+    state.linkedin=action.payload.linkedin
+    state.img=action.payload.img
+    state.intrests=action.payload.intrests
+    state.languages=action.payload.languages
+    state.frameworks=action.payload.frameworks
+    state.isauth=action.payload.isauth
+   },
+  },
+})
+
+export const { add } = userSlice.actions;
+export default userSlice;
