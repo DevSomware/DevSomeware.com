e6766fe20d1b6dd8b0f08afb7e9a9d6d5b7268d9
Aniket-Subudh1 <aniketsubudhi00@gmail.com>
2024-12-02 13:17:07 +0530

    Minor changes

--- src/app/verification/page.tsx
+++ src/app/verification/page.tsx
@@ -1,35 +1,44 @@
 "use client";
 import React from "react";
+import { BackgroundLines } from "@/components/ui/background-lines";
 import { motion } from "framer-motion";
-import { LampContainer } from "@/components/ui/lamp";
-
-const page = () => {
+import { Button } from "@/components/Button";
+import Link from "next/link";
+const VerificationPage = () => {
   return (
-    <LampContainer>
-      <AnimatedText />
-    </LampContainer>
+    <BackgroundLines className="flex items-center min-h-screen sm:-mt-[5rem] justify-center w-full flex-col px-4">
+      <motion.h2
+        className="bg-clip-text text-transparent text-center bg-gradient-to-b from-neutral-900 to-neutral-700 dark:from-neutral-600 dark:to-white text-2xl md:text-4xl lg:text-7xl font-sans py-2 md:py-10 relative z-20 font-bold tracking-tight"
+        initial={{ opacity: 0, y: -50 }}
+        animate={{ opacity: 1, y: 0 }}
+        transition={{
+          duration: 0.8,
+          ease: "easeInOut",
+        }}
+      >
+        Application Under Verification
+      </motion.h2>
+      <motion.p
+        className="max-w-xl mx-auto text-sm md:text-xl text-neutral-700 dark:text-neutral-400 text-center"
+        initial={{ opacity: 0, y: 50 }}
+        animate={{ opacity: 1, y: 0 }}
+        transition={{
+          duration: 1,
+          delay: 0.3,
+          ease: "easeInOut",
+        }}
+      >
+        Thank you for applying to join <strong>DevSomeware</strong>. Your
+        application is currently under verification. Our team is reviewing your
+        submission, and you will receive an update soon. We appreciate your
+        patience and look forward to having you as part of our community.
+      </motion.p>
+      <br></br>
+      <Link href="/contact">
+        <Button>CONTACT US</Button>
+      </Link>
+    </BackgroundLines>
   );
 };
 
-const AnimatedText = () => {
-  return (
-    <motion.h1
-      initial={{ opacity: 0.8, y: 50 }}
-      animate={{
-        opacity: [0.8, 1, 0.8],
-        scale: [1, 1.05, 1],
-        y: [50, 45, 50],
-      }}
-      transition={{
-        repeat: Infinity,
-        duration: 3,
-        ease: "easeInOut",
-      }}
-      className="mt-8 bg-gradient-to-br from-slate-300 to-slate-500 sm:-mt-[10rem] lg:mt-10 py-4 bg-clip-text text-center text-4xl font-medium tracking-tight text-transparent md:text-7xl"
-    >
-      Your Application <br /> is under Verification ...
-    </motion.h1>
-  );
-};
-
-export default page;
+export default VerificationPage;
