ea32b89fe7e7609b905ea7ce17210a703bb37055
Basir Khan <khanbasir5555@gmail.com>
2024-12-02 15:29:19 +0530

    redux added

--- src/app/StoreProvider.tsx
+++ src/app/StoreProvider.tsx
@@ -2,7 +2,7 @@
 import { useRef } from 'react'
 import { Provider } from 'react-redux'
 import { makeStore, AppStore } from '../lib/store'
-import {add} from '../lib/features/user/userSlice'
+// import {add} from '../lib/features/user/userSlice'
 export default function StoreProvider({
   children
 }: {
@@ -13,17 +13,17 @@ export default function StoreProvider({
     // Create the store instance the first time this renders
     storeRef.current = makeStore()
     //add intial data here
-    storeRef.current.dispatch(add({
-        name:"DevSomeware",
-        email:"khanbasir@gmail.com",
-        github:"dsfs",
-        linkedin:"dsfs",
-        img:"dsfs",
-        intrests:["dsfs"],
-        languages:"dsfs",
-        frameworks:"dsfs",
-        isauth:false,
-    }))
+    // storeRef.current.dispatch(add({
+    //     name:"DevSomeware",
+    //     email:"khanbasir@gmail.com",
+    //     github:"dsfs",
+    //     linkedin:"dsfs",
+    //     img:"dsfs",
+    //     intrests:["dsfs"],
+    //     languages:"dsfs",
+    //     frameworks:"dsfs",
+    //     isauth:false,
+    // }))
 
   }
 
